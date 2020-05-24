77170ea1db75b1fb7e74cdd0f1aff0c9c0f43d78
Walter <relw1wa-github@yahoo.de>
2020-05-24 10:53:35 +0200

    Refactor usage of FontAwesome icons

--- components/life-game.js
+++ components/life-game.js
@@ -1,4 +1,4 @@
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 
 import LifeSimulation from './life-simulation/life-simulation';
